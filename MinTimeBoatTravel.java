113b61966829df95ac512c95d9b08c6604ec7d48
parzival <45847904+sanchit05@users.noreply.github.com>
2020-10-07 18:39:29 +0530

    Minimum time taken problem
    The minimum time is taken by boat to take all people along with bags program using dynamic programing

---
+++ MinTimeBoatTravel.java
@@ -0,0 +1,36 @@
+public class MinTimeBoatTravel {
+
+    static int min(int a, int b){
+        return (a<b) ? a:b;
+    }
+
+    static int mintime(int wt[], int w, int n){
+
+        if(n==0||w==0){
+            return 0;
+        }
+
+        if(wt[n-1] <= w){
+            return min(15 + mintime(wt, w-wt[n-1], n-1), mintime(wt, w, n-1));
+        }else  if(wt[n-1]> w){
+            return  mintime(wt, w, n-1);
+        }
+
+
+        return w;
+    }
+
+    public static void main(String[] args){
+
+        int[] wt = new int[]{70,68,97,50,40,85,60,70,60,30,60,20,10};   
+        int w=140;
+        int n=13;
+
+        System.out.println(mintime(wt,val,w,n));
+
+    }
+
+}
+
+
+
